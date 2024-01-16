a53b2fa27e49c3de956606c684ca427e93570981
dk949 <56653556+dk949@users.noreply.github.com>
2024-01-16 15:54:00 +0000

    added lightmode toggle

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ require "plugins" ()
 
 vim.api.nvim_create_autocmd("User", {
     pattern = "GoyoLeave",
-    command="norm! ZZ",
+    command = "norm! ZZ",
 })
 
 vim.api.nvim_create_autocmd("User", {
     pattern = "GoyoEnter",
-    callback=function()
+    callback = function()
         vim.opt.statusline = [[%=%{WordCount()}]]
-        vim.cmd[[hi statusline ctermfg=white guifg=#abb2bf guibg=#2c2d30]]
+        vim.cmd [[hi statusline ctermfg=white guifg=#abb2bf guibg=#2c2d30]]
     end,
 })
 -- tabs
@@ -37,7 +37,7 @@ vim.opt.textwidth = 80
 vim.opt.wrap = true
 
 -- mouse
-vim.opt.mouse=nil
+vim.opt.mouse = nil
 
 -- spelling
 vim.opt.spell = true
@@ -62,19 +62,22 @@ vim.keymap.set({ 'v', 'n', 'o' }, "$", "g$", { silent = true })
 vim.keymap.set({ 'v', 'n', 'o' }, "g0", "0", { silent = true })
 vim.keymap.set({ 'v', 'n', 'o' }, "g$", "$", { silent = true })
 
-vim.keymap.set('n',  "A", "g$a", { silent = true })
-vim.keymap.set('n', "gA",   "A", { silent = true })
-vim.keymap.set('n',  "I", "g0i", { silent = true })
-vim.keymap.set('n', "gI",   "I", { silent = true })
+vim.keymap.set('n', "A", "g$a", { silent = true })
+vim.keymap.set('n', "gA", "A", { silent = true })
+vim.keymap.set('n', "I", "g0i", { silent = true })
+vim.keymap.set('n', "gI", "I", { silent = true })
 
-vim.keymap.set('n',  "G", "Gzz", {silent = true })
-vim.keymap.set('n', "vv",   "V", {silent = true })
-vim.keymap.set('n',  "V",  "v$", {silent = true })
-vim.keymap.set('n',  "Y",  "y$", {silent = true })
+vim.keymap.set('n', "G", "Gzz", { silent = true })
+vim.keymap.set('n', "vv", "V", { silent = true })
+vim.keymap.set('n', "V", "v$", { silent = true })
+vim.keymap.set('n', "Y", "y$", { silent = true })
 
-vim.keymap.set('v', "<C-C>", [["*y :let @+=@*<CR>]], {silent = true })
+vim.keymap.set('v', "<C-C>", [["*y :let @+=@*<CR>]], { silent = true })
 
-vim.keymap.set('n' , "<leader>m"  , ":Make!<CR>"                  , { })
-vim.keymap.set('n' , "<leader>s"  , ":update<CR>"                 , { silent = true })
-vim.keymap.set('n' , "<leader>b"  , ":e #<CR>"                    , { silent = true })
-vim.keymap.set('n' , "<leader>nn" , ":NvimTreeFindFileToggle<CR>" , { silent = true })
+vim.keymap.set('n', "<leader>m", ":Make!<CR>", {})
+vim.keymap.set('n', "<leader>s", ":update<CR>", { silent = true })
+vim.keymap.set('n', "<leader>b", ":e #<CR>", { silent = true })
+vim.keymap.set('n', "<leader>nn", ":NvimTreeFindFileToggle<CR>", { silent = true })
+
+
+vim.keymap.set('n', "<leader>o", OdToggle, { silent = true })
--- lua/onedark_config.lua
+++ lua/onedark_config.lua
@@ -1,12 +1,42 @@
+-- these have to be global, to be accessible after packer compiles this config
+
+OD_dstyle = 'warm'
+OD_lstyle = 'light'
+OD_current = OD_dstyle
+
+function OD_getHighlights(style)
+    local p = require('onedark.palette')
+    return {
+        SpellBad    = { fg = '$red' },
+        SpellCap    = { fg = '$cyan' },
+        SpellLocal  = { fg = "$orange" },
+        NormalFloat = { fg = p[style].fg, bg = p[style].bg_d },
+    }
+end
+
+function OdToggle()
+    local od = require('onedark')
+    -- NOTE: This is set *BEFORE* changing the OD_current style
+    --       I'm not sure why
+    od.set_options('highlights', OD_getHighlights(OD_current))
+    if OD_current == OD_dstyle then OD_current = OD_lstyle else OD_current = OD_dstyle end
+    od.toggle()
+    od.load()
+end
+
 return function()
-    require('onedark').setup {
+    local od = require('onedark')
+    -- 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' or 'light'
+
+    od.setup {
         -- Main options --
-        style = 'warm', -- 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' or 'light'
+        style = OD_current,
         transparent = false,
         term_colors = true,
         ending_tildes = false,
         cmp_itemkind_reverse = true,
 
+        toggle_style_list = { OD_dstyle, OD_lstyle },
 
         -- italic, bold, underline, none
         -- or any combination e.g. 'italic,bold'
@@ -22,18 +52,14 @@ return function()
             transparent = false, -- lualine center bar transparency
         },
 
-        colors = {},     -- Override default colors
-        highlights = {
-            SpellBad  = { fg = '$red'    },
-            SpellCap  = { fg = '$cyan'   },
-            SpellLocal= { fg = "$orange" },
-        }, -- Override highlight groups
+        colors = {},                              -- Override default colors
+        highlights = OD_getHighlights(OD_lstyle), -- Override highlight groups
 
         diagnostics = {
-            darker = true,     -- darker colors for diagnostic
-            undercurl = true,  -- use undercurl instead of underline for diagnostics
+            darker     = true, -- darker colors for diagnostic
+            undercurl  = true, -- use undercurl instead of underline for diagnostics
             background = true, -- use background color for virtual text
         },
     }
-    require('onedark').load()
+    od.load()
 end
